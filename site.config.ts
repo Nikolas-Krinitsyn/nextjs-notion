98703055f944bfb202a66a11f3fe1855813fd0c6
Nikolas-Krinitsyn <59940130+Nikolas-Krinitsyn@users.noreply.github.com>
2023-04-23 14:26:12 +0545

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1cb7fc1327754116b10a24cb27e24415',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Nikolas Krinitsyn',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Nikolas Krinitsyn',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
